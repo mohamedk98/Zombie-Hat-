2047e8707adef53bbef3250fd27a7d58fa60aa97
Ali Maher <alimahermahmoud@gmail.com>
2022-07-23 20:01:40 +0200

    Starting clean

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { authorization } = require("../middlwares/authentication");
-const router = express.Router();
-const {getProfile} = require("../controllers/userController");
-
-/* GET user profile  */
-router.get("/profile", getProfile);
-
-module.exports = router;
